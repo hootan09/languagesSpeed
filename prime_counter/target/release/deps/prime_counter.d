6117a0cc5f15a2ea26214a9a7fb3565147f256be
niki <hootan09@gmail.com>
2022-01-15 15:12:08 +0330

    Rust Optimization

--- prime_counter/target/release/deps/prime_counter.d
+++
@@ -1,5 +0,0 @@
-C:\Users\niki\Desktop\code test\prime_counter\target\release\deps\prime_counter.exe: src\main.rs
-
-C:\Users\niki\Desktop\code test\prime_counter\target\release\deps\prime_counter.d: src\main.rs
-
-src\main.rs:
